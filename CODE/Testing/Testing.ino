9fe92ef59be89ff689e054c5e3aa772a02c4f247
EDO <eduzyolo@gmail.com>
2023-05-04 09:33:06 +0200

    added test test code

--- CODE/Testing/Testing.ino
+++ CODE/Testing/Testing.ino
@@ -1,18 +1,26 @@
+#include "Arduino.h"
 
-#define TEST_BUZZER 
+// #define TEST_BUZZER 
+#define TEST_TEMP
 
 #ifdef TEST_BUZZER
 #include "Buzzer.h"
+#elif defined(TEST_TEMP)
+#include "Temp.h"
 #endif
 
 void setup() {
 #ifdef TEST_BUZZER
   setup_buzzer();
+#elif defined(TEST_TEMP)
+  setup_temp();
 #endif
 }
 
 void loop() {
 #ifdef TEST_BUZZER
   loop_buzzer();
+#elif defined(TEST_TEMP)
+  loop_temp();
 #endif
 }
